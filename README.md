38a13a00a59c97873d700b4238c842bb30acbbc5
Asaedry <marshallchrismiller@gmail.com>
2022-03-09 16:23:36 -0800

    fixed a few things, nothing major

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Feel Good Messages!
+Feel Good Messages!
 --
 
 Just some messages that you can go through to help you get through the day. 
--- resources/javascript/mixedMessages.js
+++ resources/javascript/mixedMessages.js
@@ -6,7 +6,9 @@ const messages = [
     "Someone probably think's you're great, I'm sure.",
     "You don't smell that bad, like it could be worse.",
     "You're alive at least, right?",
-    "I bet you have... like... eyes, that's something."
+    "I bet you have... like... eyes, that's something.",
+    "I'm sure you have nice shoes... Or just shoes, at least...",
+    "Your mom probably says nice things about you."
 ]
 
 let lastNum = null; 
@@ -15,7 +17,6 @@ const randomMessage = () => {
     if(lastNum != num){
         message.innerHTML = messages[num];
         lastNum = num   
-        console.log(lastNum); 
     } else {
         randomMessage();
     }
